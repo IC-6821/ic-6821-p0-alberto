31fa71d52ef57c806f9cba1d1b01f3971eac1d80
JP113z <josepablof113@gmail.com>
2024-08-13 22:31:54 -0600

    evitar repetidos en computer

--- app/src/main/java/org/example/ComputerPlayer.java
+++ app/src/main/java/org/example/ComputerPlayer.java
@@ -3,10 +3,19 @@ package org.example;
 import java.util.Random;
 
 public class ComputerPlayer extends Player {
+
+    Board board = new Board();
     public int[] getComputerCoordinates () {
         Random random = new Random();
         int row = random.nextInt(3);
         int col = random.nextInt(3);
+        while (true) {
+            if (Game.isValidPosition(row, col, board.board)) {
+                break;
+            }
+            row = random.nextInt(3);
+            col = random.nextInt(3);
+        }
         return new int[]{row, col};
     }
 }
