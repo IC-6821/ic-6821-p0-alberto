87d99b385d6c6cd0f7239fe055e4d6af92dd5889
Nico-skz <75637652+Nico-skz@users.noreply.github.com>
2024-08-20 17:20:01 -0600

    Cambios en coordinate

--- app/src/main/java/org/example/Coordinate.java
+++ app/src/main/java/org/example/Coordinate.java
@@ -4,17 +4,17 @@ public class Coordinate {
     private int row;
     private int column;
 
+    public Coordinate(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
     public int getRow() {
         return row;
     }
 
     public int getColumn() {
         return column;
-    }
-
-    public Coordinate(int row, int column) {
-        this.row = row;
-        this.column = column;
     }
 
 }
